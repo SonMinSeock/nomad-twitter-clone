be2d436b0101feb6d00d61ae9d295ca67541162b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-10-28 08:48:08 +0900

    Navigation Bar

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
-
 function Home() {
-  const navigate = useNavigate();
-  const logOut = async () => {
-    await auth.signOut();
-    navigate("/login");
-  };
   return (
     <>
-      <h1>
-        <button onClick={logOut}>로그아웃</button>
-      </h1>
+      <h1>Home</h1>
     </>
   );
 }
